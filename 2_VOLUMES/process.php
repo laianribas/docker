9ce8f5e73b68844110d2e0651cdc5e811b7cc98a
laianribas <laian67a@gmail.com>
2022-04-23 10:41:55 -0300

    finalização dos volumes

--- 2_VOLUMES/process.php
+++ 2_VOLUMES/process.php
@@ -1,16 +1,15 @@
 <?php
 
-  $message = $_POST["message"];
+$message = $_POST["message"];
 
-  $files = scandir("./messages");
+$files = scandir("./messages");
   $num_files = count($files) - 2; // . e ..
 
   $fileName = "msg-{$num_files}.txt";
 
   $file = fopen("./messages/{$fileName}", "x");
+  header("Location: index.php");
 
   fwrite($file, $message);
 
   fclose($file);
-
-  header("Location: index.php"); 
